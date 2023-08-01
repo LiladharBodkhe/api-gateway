6f94ca80572be119f58bfde79689fd4735ca2252
unknown <liladharbodkhe75@gmail.com>
2023-08-01 23:38:06 +0530

    Api gateway

---
+++ src/test/java/com/liladhar/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liladhar.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
